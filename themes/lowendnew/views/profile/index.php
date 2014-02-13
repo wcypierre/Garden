3e98b28877d415f2e6375712cde8decb0169356a
Maarten Kossen <hello@mpkossen.com>
2014-02-13 15:28:55 +0100

    Added the _current_ version of the Vanilla 2.2 port of the LowEndTalk theme.

---
+++ themes/lowendnew/views/profile/index.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit(); ?>
+<div class="Profile">
+   <?php
+   include($this->FetchViewLocation('user'));
+   // include($this->FetchViewLocation('tabs'));
+   // echo Gdn_Theme::Module('ProfileFilterModule');
+   echo "<br />";
+   include($this->FetchViewLocation($this->_TabView, $this->_TabController, $this->_TabApplication));
+   ?>
+</div>
